2f5dd7c944c66ec3e31b1d39903e39a160cabdfd
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2023-05-08 12:39:13 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ActivitySegment {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Location {
+    name: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Duration {
+    #[serde(rename="startTimestamp")]
+    start: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PlaceVisit {
+    location: Location,
+    duration: Duration
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum TimelineEvent {
+    #[serde(rename="activitySegment")]
+    ActivitySegment(ActivitySegment),
+    #[serde(rename="placeVisit")]
+    PlaceVisit(PlaceVisit)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    #[serde(rename="timelineObjects")]
+    timeline: Vec<TimelineEvent>
+}
+
+fn main() {
+    let data: Data = serde_json::from_slice(&std::fs::read("2023_APRIL.json").unwrap()).unwrap();
+    let work_days = data.timeline.iter().cloned().filter_map(|event| match event {
+        TimelineEvent::PlaceVisit(visit) => Some(visit),
+        _ => None
+    }).filter(|visit| visit.location.name.is_some() && visit.location.name.as_ref().unwrap() == "Eight Bells Framing and Gallery").collect::<Vec<PlaceVisit>>();
+
+    println!("Worked for {} days", work_days.len());
+    work_days.iter().for_each(|day| {
+        println!("{}", &day.duration.start[0..10]);
+    })
+}
